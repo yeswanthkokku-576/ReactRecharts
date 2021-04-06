bb23acb160a6ef66f33ccb64e9ef59aef58e745c
yeswanth kokku <yeswanthkokku@yeswanths-MacBook-Air.local>
2021-04-06 20:07:29 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 import './App.css';
+import { useInterval } from './utilities/useInterval';
 
 function App() {
+  const [data, loadData] = useState(null);
+
+  useInterval(async () => {
+    console.log(`Calling backend api`);
+    const resp = await axios({headers: {'Access-Control-Allow-Origin':'*'},method: 'get', url:'http://localhost:8080/api/testing'});
+     resp.data.sort(function(a, b) {
+       var keyA = new Date(a.time.concat("-01"));
+        var keyB = new Date(b.time.concat("-01"));
+       // Compare the 2 dates
+       if (keyA < keyB) return -1;
+       if (keyA > keyB) return 1;
+       return 0;
+     });
+    loadData(resp.data);
+  },5000);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <LineChart width={900} height={300} data={data}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5
+        }}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="time" />
+        <YAxis type ="number" tickCount ={6} domain = {[0,100]}/>
+        <Tooltip />
+        <Legend />
+        <Line type="monotone" dataKey="data" stroke="#8884d8" strokeWidth={2} />
+      </LineChart>
   );
 }
 
